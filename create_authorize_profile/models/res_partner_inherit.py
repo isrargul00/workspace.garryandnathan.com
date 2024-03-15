d0ae5b4b39fa77ac9d487cccd807755cb7039716
Israr <isrargul00@gmail.com>
2024-03-15 16:22:35 +0300

    create

--- create_authorize_profile/models/res_partner_inherit.py
+++ create_authorize_profile/models/res_partner_inherit.py
@@ -14,20 +14,9 @@ class ResPartnerExt(models.Model):
             'views': [(view.id, 'form')],
             'view_id': view.id,
             'target': 'new',
-            'context': {'default_first_name': self.name,
-                        'default_last_name': self.name,
-                        'default_phone': self.phone,
-                        'default_street': self.street,
-                        'default_city': self.city,
-                        'default_state': self.state_id.name,
-                        'default_zip': self.zip,
-                        'default_email': self.email,
-                        # 'default_code': self.code,
-                        'default_partner_id': self.id,
-                        'default_country': self.country_id.code,
-                        # 'default_cc_number': "4111111111111111",
-                        }
+            'context': {
+                'default_email': self.email,
+                'default_partner_id': self.id,
+            }
 
         }
-
-# 'context': self.env.context,
